8caca5b6691b7d4aa208d63632fb5753728c791f
Cheery <838529779@qq.com>
2017-09-15 12:58:32 +0800

    完成DEMO13的测试用例

---
+++ DOM-DEMO/DEMO13/tests.js
@@ -0,0 +1,14 @@
+describe('测试节点内容', function () {
+
+    it('测试TextNode的CRUD', () => {
+        var ul = document.getElementById('list')
+
+        var li = document.createElement('li')
+        var text = document.createTextNode('content4')
+        li.appendChild(text)
+        ul.appendChild(li)
+
+        should(ul.children.length).exactly(4, 'ul元素中的li子节点长度应为4')
+    })
+
+})
