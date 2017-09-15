78285b735342695947e7fffa1a04b65a5295ce6a
Cheery <838529779@qq.com>
2017-09-15 12:54:55 +0800

    完成DEMO12的测试用例

---
+++ DOM-DEMO/DEMO12/tests.js
@@ -0,0 +1,18 @@
+describe('测试节点内容', function () {
+
+    it('测试TextNode的CRUD', () => {
+        var ul = document.getElementById('list')
+
+        var li = document.createElement('li')
+        var text = document.createTextNode('content4')
+        text.appendData('appendTest')
+        text.deleteData('content4append'.length, 4)
+        text.insertData(0, 'start')
+        text.replaceData(0, 'start'.length, 'new')
+        li.appendChild(text)
+        ul.appendChild(li)
+
+        text.splitText(3)
+    })
+
+})
